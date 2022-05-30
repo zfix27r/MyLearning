592c965d8f5c3f54d1f3c4903a920bc65dc14e02
Sergey Zabelin <zfix.amursk@ya.ru>
2022-05-30 13:45:34 +1000

    Начало обучения. Добавил конспект "чистой архитектуры для андройд разработчика от Mail.ru group"

---
+++ app/src/main/java/ru/sergeyzabelin/mylearning/entities/LessonTopic.kt
@@ -0,0 +1,10 @@
+package ru.sergeyzabelin.mylearning.entities
+
+data class LessonTopic(
+    val id: Int,
+
+    val title: String,
+    val description: String,
+    val content: String,
+    val image: String
+)
